e4ae62690b592f39ac6217e133f216b9c4c5d209
torhugo <arruda.victorhugo@outlook.com>
2023-11-08 01:07:06 -0300

    INITIAL PROJECT: - Adding basic structure of project.

---
+++ src/main/java/dev/torhugo/ekanrest/EkanRestApplication.java
@@ -0,0 +1,13 @@
+package dev.torhugo.ekanrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkanRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkanRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/torhugo/ekanrest/EkanRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.torhugo.ekanrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkanRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
